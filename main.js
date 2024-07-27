0756481ea9aa5534fbd1469638a229ae89bd8222
Elli junior <ellijuniorsantosdejesus8@gmail.com>
2024-07-26 21:27:42 -0300

    Update main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-    const perguntarNome = prompt('Qual seu nome ?');
-    if (perguntarNome == 0){
-        aler("Chatoooo");
-    } else{
-        alert("olá seja bem vindo "+ perguntarNome);
-    }
